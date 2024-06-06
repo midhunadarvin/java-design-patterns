7cd3b80a1306d279fa4015b8b1681f77dabed819
vishal-joshy <vishalvjjoshy@gmail.com>
2024-06-06 18:54:18 +0530

    Revert "Add template engine"
    This reverts commit 38d44af8f20831d6fece6a52d9390d0e06eb49b4.

--- plugin-architecture/plugins/plugin-maven/src/main/java/com/chistadata/plugins/Main.java
+++ plugin-architecture/plugins/plugin-maven/src/main/java/com/chistadata/plugins/Main.java
@@ -1,14 +1,9 @@
 package com.chistadata.plugins;
-import freemarker.template.Configuration;
-import freemarker.template.Template;
-import freemarker.template.TemplateException;
 
-import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.io.Writer;
-import java.util.HashMap;
-import java.util.Map;
-
+/**
+ * Hello world!
+ *
+ */
 public class Main
 {
     public boolean PreExecute() {
@@ -16,18 +11,8 @@ public class Main
         return true;
     }
 
-    public boolean Execute() throws IOException, TemplateException {
-        String templateRoot = "/Users/vishal/source/java-design-patterns/plugin-architecture/plugins/plugin-maven/src/templates";
-
-        Configuration cfg = TemplateEngine.init(templateRoot);
-
-        Map<String, Object> dm = new HashMap<>();
-        dm.put("user","John Doe");
-
-        Template temp = cfg.getTemplate("test.ftlh");
-
-        Writer out = new OutputStreamWriter(System.out);
-        temp.process(dm, out);
+    public boolean Execute() {
+        System.out.println("Execute: plugin maven");
         return true;
     }
 
--- plugin-architecture/plugins/plugin-maven/src/main/java/com/chistadata/plugins/TemplateEngine.java
+++ plugin-architecture/plugins/plugin-maven/src/main/java/com/chistadata/plugins/TemplateEngine.java
@@ -1,22 +1,2 @@
-package com.chistadata.plugins;
-
-import freemarker.template.Configuration;
-import freemarker.template.TemplateExceptionHandler;
-
-import java.io.File;
-import java.io.IOException;
-import java.util.TimeZone;
-
-public class TemplateEngine {
-    public static Configuration init(String templateFolderPath) throws IOException {
-        Configuration cfg = new Configuration(Configuration.VERSION_2_3_32);
-        cfg.setDirectoryForTemplateLoading(new File(templateFolderPath));
-        cfg.setDefaultEncoding("UTF-8");
-        cfg.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
-        cfg.setLogTemplateExceptions(false);
-        cfg.setWrapUncheckedExceptions(true);
-        cfg.setFallbackOnNullLoopVariable(false);
-        cfg.setSQLDateAndTimeTimeZone(TimeZone.getDefault());
-        return cfg;
-    }
+package com.chistadata.plugins;public class TemplateEngine {
 }
